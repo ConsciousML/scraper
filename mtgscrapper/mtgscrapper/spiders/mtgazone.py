58a89798d3a159a9fc4fb094b57b4c35484b825d
Axel Mendoza <axel.mendoza@getboxy.co>
2023-01-23 18:02:26 +0100

    mtgazone: able to scrap first article page

--- mtgscrapper/mtgscrapper/spiders/mtgazone.py
+++ mtgscrapper/mtgscrapper/spiders/mtgazone.py
@@ -6,5 +6,29 @@ class MTGArenaZoneSpider(scrapy.Spider):
     start_urls = ['https://mtgazone.com/articles/']
 
     def parse(self, response):
-        for article in response.xpath('//article'):
-            print(article)
+        for article_selector in response.xpath(
+                '//article[contains(@class, "entry-card post")]'):
+
+            title_selector = article_selector.xpath('h2[@class="entry-title"]')
+
+            article_url = title_selector.xpath('a/@href').get()
+            article_title = title_selector.xpath('a/text()').get().strip()
+
+            article_tags = article_selector.xpath(
+                './ul[@data-type="simple:none"]/li/a/text()').getall()
+
+            author_date_selector = article_selector.xpath(
+                './ul[@data-type="icons:none"]')
+            author_name = author_date_selector.css('span').xpath(
+                './text()').get()
+            article_date = author_date_selector.css('time').xpath(
+                './text()').get()
+
+            yield {
+                'id': article_selector.attrib['id'],
+                'url': article_url,
+                'title': article_title,
+                'tags': article_tags,
+                'author': author_name,
+                'date': article_date
+            }
